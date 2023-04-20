e066e19896a3c567d815b206be0bbc58a4977634
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-20 21:13:47 +0300

    first commit

---
+++ string2.js
@@ -0,0 +1,30 @@
+const sentence = "my father hits the ball with the hurting leg"
+const l = sentence.match(the).length;
+console.length(l);
+
+//finding elements
+const string1 = "shes coming from the market"
+const c = string1.search;
+console.log(c);
+
+const fruits = ["mango","orange","apple","pineapple","coconut"]
+fruits.pop(orange);
+return(fruits);
+
+const girls = ["ceccile","ann","joy","hanna"];
+const myboys =["lemmie","remmie","antony"];
+const myChildren = myGrls.concat(myboys);
+
+
+let x = "amazing";
+let x1 = x.toLowerCase();
+console.log(x1);
+
+let y = `beneath`;
+let y1 = y.toLowerCase();
+console.log(y1);
+
+
+
+
+
